4c31f3160209512647dd9736da10a2e55dc46396
kamucell <kamuranseckin@hotmail.com>
2024-06-24 22:14:25 +0300

    developed future

--- MediatR_CQRS/SuperTrader.Service/Future/Share/MappingProfile/Class1.cs
+++ MediatR_CQRS/SuperTrader.Service/Future/Share/MappingProfile/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SuperTrader.Service.Future.Share.Query
+namespace SuperTrader.Service.Future.Share.MappingProfile
 {
     internal class Class1
     {
